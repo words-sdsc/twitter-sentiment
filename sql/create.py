3306378387d07da2c14cac3a6cbf49843a609121
Karl <Kouralr@gmail.com>
2017-07-17 00:00:42 -0700

    Creates db

---
+++ sql/create.py
@@ -0,0 +1,19 @@
+import sqlite3
+
+conn = sqlite3.connect('TweetInfo.db')
+
+conn = conn.cursor()
+
+# Create table
+
+conn.execute('''CREATE TABLE TWEET_INFO
+         (ID            INT PRIMARY KEY NOT NULL,
+         DATE           TEXT            NOT NULL,
+         EPOCH          INT             NOT NULL,
+         TWEET          CHAR(250)       NOT NULL,
+         SENTIMENT      INT);''')
+
+
+# We can also close the connection if we are done with it.
+# Just be sure any changes have been committed or they will be lost.
+conn.close()
