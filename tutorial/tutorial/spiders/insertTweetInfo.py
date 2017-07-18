c464968983b1de48696a4b8f99631e5be0011193
Karl <Kouralr@gmail.com>
2017-07-17 21:33:45 -0700

    Changed temp table name

--- tutorial/tutorial/spiders/insertTweetInfo.py
+++ tutorial/tutorial/spiders/insertTweetInfo.py
@@ -1,22 +1,15 @@
 import sqlite3
 
-def okay():
-    for i in range(1, 10):
-        print( "WORKED\n" )
-
 def insertTweetInfo( dbName, dataBaseLoc, tweetId, date, epoch, tweet, sent ):
-    # Conenct to the database
+    # Connect to specified DB
     conn = sqlite3.connect(dataBaseLoc)
 
     # Insert a row of data
-    conn.execute("INSERT INTO " + dbName + " (ID, DATE, EPOCH, TWEET, SENTIMENT) \
-       VALUES ( ?, ?, ?, ?, ? )", ( (tweetId, date, epoch, tweet, sent) ))
-
-
-    # Commit the changes made
-    conn.commit()
-
-    # close the connection
+    try:
+        conn.execute("INSERT INTO " + dbName + " (ID, DATE, EPOCH, TWEET, SENTIMENT) \
+           VALUES ( ?, ?, ?, ?, ? )", ( (tweetId, date, epoch, tweet, sent) ))
+    except:
+        print( "ERROR INSERTING INTO DB" )
+    
+    conn.commit()   
     conn.close()
-
-
