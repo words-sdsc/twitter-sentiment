ee479da97756be6d8a9297ce984947d85c46b671
Karl <Kouralr@gmail.com>
2017-07-19 14:52:57 -0700

    Starting change to NLTK over coreNLP

--- SentimentAnalysis/sentimentAnalysis.py
+++ SentimentAnalysis/sentimentAnalysis.py
@@ -1,4 +1,5 @@
 from pycorenlp import StanfordCoreNLP
+import json
 nlp = StanfordCoreNLP('http://localhost:9000')
 
 from time import sleep
@@ -20,7 +21,7 @@ TWEET_ID_INDEX = 0
 
 conn = sqlite3.connect('../sql/TweetInfo.db')
 
-cursor = conn.execute("SELECT * from TWEET_INFO_2")
+cursor = conn.execute("SELECT * from TWEET_INFO_LOL")
 
 # Go through all tweets in DB
 for row in cursor:
@@ -39,6 +40,8 @@ for row in cursor:
 
 index = 0
 
+#stringArr.append("LOL XD")
+
 # Annotate the string using coreNLP
 for string in stringArr:
     # Set up annotation 
@@ -50,25 +53,49 @@ for string in stringArr:
                })
 
     # Let nlp load
-    sleep( .5 )
+    #sleep( .5 )
 
-    # Print out the results!
+    if index == 102:
+        print( string )
+
+    if index == 101:
+        print( string )
+    if index == 103:
+        print( string )
+
+    if index == 284:
+        print( string )
+    print("Index..: %d" % index )
+
+    try:
+        for s in res.values():
+            #print( s )
+            try:
+                print( s[0]["sentimentValue"] )
+
+                str_ = "TWEET_INFO_LOL"
+
+                # Insert a row of data
+                conn.execute("UPDATE " + str_ + " set SENTIMENT = ? where ID = ?", \
+                    ( s[0]["sentimentValue"] , tweetId[index] ))
+            except:
+                x = s
+                print( json.dumps(x, indent=2) )
+    except:
+        print( res )
+        print( "okokokok" )
+
+    index += 1
+
+    """
     for s in res["sentences"]:
     # Prints( "Index: Words : Tokens : Sent Value : Sentiment (Pos/Neg/Neutral)" )
-        print ( "%d: '%s': %s %s\n" % ( index, " ".join([t["word"] for t in s["tokens"]]),\
-                s["sentimentValue"], s["sentiment"]) )
+        #print ( "%d: '%s': %s %s\n" % ( index, " ".join([t["word"] for t in s["tokens"]]),\
+        #        s["sentimentValue"], s["sentiment"]) )
         
         # Connect to database
-
-        str_ = "TWEET_INFO_2"
-
-        # Insert a row of data
-        conn.execute("UPDATE " + str_ + " set SENTIMENT = ? where ID = ?", \
-                ( s["sentimentValue"] , tweetId[index] ))
-        print( tweetId )
-        
-
-        index += 1
+        print("FOREACH: %s" % s)
+    """
 
 conn.commit()
 conn.close()
