2ca19154128c89756f0876f989877b79b7e6de89
Karl <Kouralr@gmail.com>
2017-07-22 19:10:12 -0700

    Changed prints for corgis

--- sql/create.py
+++ sql/create.py
@@ -1,19 +1,29 @@
 import sqlite3
 
-conn = sqlite3.connect('TweetInfo.db')
+def addTable( dbName, tableName ):
+    conn = sqlite3.connect(dbName)
 
-conn = conn.cursor()
+    conn = conn.cursor()
 
-# Create table
+    try:
+        # Create table
+        conn.execute('''CREATE TABLE %s
+                 (ID            INT PRIMARY KEY UNIQUE  NOT NULL,
+                 DATE           INT                     NOT NULL,
+                 TWEET          CHAR(250)               NOT NULL,
+                 SENTIMENT      REAL);''' % tableName)
 
-conn.execute('''CREATE TABLE TWEET_INFO_LOL
-         (ID            INT PRIMARY KEY UNIQUE  NOT NULL,
-         DATE           TEXT                    NOT NULL,
-         EPOCH          INT                     NOT NULL,
-         TWEET          CHAR(250)               NOT NULL,
-         SENTIMENT      INT);''')
+    except:
+        print("Table: %s already exists in Database: %s." % tableName, dbName ) 
 
+    # We can also close the connection if we are done with it.
+    # Just be sure any changes have been committed or they will be lost.
+    conn.close()
 
-# We can also close the connection if we are done with it.
-# Just be sure any changes have been committed or they will be lost.
-conn.close()
+def createDB( dbName ):
+    try:
+        conn = sqlite3.connect(dbName)
+    except:
+        print("Error creating %s\n" % dbName)
+
+addTable( "TweetInfo.db", "Test_run" )
--- sql/print.py
+++ sql/print.py
@@ -7,14 +7,13 @@ conn = sqlite3.connect('TweetInfo.db')
 
 print( "Opened database successfully" )
 
-cursor = conn.execute("SELECT * from TWEET_INFO_LOL")
+cursor = conn.execute("SELECT * from CORGIS")
 
 for row in cursor:
     print( "Tweet Id: \t%s" % row[0] )
     print( "Date: \t\t%s" % row[1] )
-    print( "Epoch: \t\t%s" % row[2] )
-    print( "Tweet Text: \t%s" % row[3] )
-    print( "Sentiment: \t%s" % row[4] )
+    print( "Tweet Text: \t%s" % row[2] )
+    print( "Sentiment: \t%s" % row[3] )
     print()
 
 print( "Operation done successfully" )
