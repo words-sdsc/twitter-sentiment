2469a8f30da02a62ce384e83efa12d15f6a285c0
Karl <Kouralr@gmail.com>
2017-08-01 16:11:56 -0700

    Sorting tweets before used with highCharts

--- webserver.py
+++ webserver.py
@@ -11,25 +11,38 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def index():
+    conn = sqlite3.connect('sql/TweetInfo.db')
     tableNamesList = conn.execute("select name from sqlite_master where type = 'table'")
     return render_template("index.html", tableNamesList=tableNamesList)
 
-@app.route('/resultsTest', methods=['POST','GET'])
-def resultsTest():
-    
-
 @app.route('/results', methods=['POST','GET'])
 def results():
-    conn = sqlite3.connect('sql/TweetInfo.db')
+    # Dispaly results
+    try:
+        # Connect to database
+        conn = sqlite3.connect('sql/TweetInfo.db')
 
-    cursor = conn.execute("SELECT * FROM _H_UNITEDAIRLINES")
-    tweetsList = []
-    index = 0
-    for row in cursor:
-        tweetsList.append([row[1], 100*row[3]])
-        index += 1
-    tweetsList.reverse()
-    return render_template("results.html", tweetsList=tweetsList)
+        # Get the chosen table
+        table_choice = request.form.get('table_choice')
+
+        # Select chosen table
+        cursor = conn.execute("SELECT * FROM " + str(table_choice[2:-3]))
+
+        tweetsList = []
+        index = 0
+        for row in cursor:
+            tweetsList.append([row[1]*1000, 100*row[3]])
+            index += 1
+
+        # Must sort becuase tweets can come out of order. FIXME Fix this at an earlier
+        # part
+        tweetsList.sort()
+        #tweetsList.reverse()
+
+        return render_template("results.html", tweetsList=tweetsList,\
+                table_choice=table_choice[2:-3])
+    except:
+        return render_template("error.html")
 
 # Starts up the webserver
 if __name__ == "__main__":
