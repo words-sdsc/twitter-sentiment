9e9f8c351327e1e8cbd632251108e96869d0c960
Karl <Kouralr@gmail.com>
2017-08-14 13:46:40 -0700

    Twitter auth keys

---
+++ keys.py
@@ -0,0 +1,9 @@
+#KEYS
+def consumer_key():
+    return 'YOUR CONSUMER KEY'
+def consumer_secret():
+    return 'YOUR CONSUMER SECRET'
+def access_token():
+    return 'YOUR ACCESS TOKEN'
+def access_token_secret():
+    return 'YOUR ACCESS TOKEN SECRET'
