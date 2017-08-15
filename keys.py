39e6e69ffbc0a7e5fc48b37f420e474cc9436f74
Karl <Kouralr@gmail.com>
2017-08-15 13:45:36 -0700

    .

--- keys.py
+++ keys.py
@@ -1,9 +1,9 @@
 #KEYS
 def consumer_key():
-    return 'YOUR CONSUMER KEY'
+    return 'qnR6rNmtAtqFOWt7sBbAq2MAY'
 def consumer_secret():
-    return 'YOUR CONSUMER SECRET'
+    return 'EtPJTIRhLDCbN5u1oUumzJJq1SrY5bXgxX7q28xeUzCx6QtA9A'
 def access_token():
-    return 'YOUR ACCESS TOKEN'
+    return '752673922399801344-fMQUutKVKNLGsw6NgyQwBOhyc7nwQme'
 def access_token_secret():
-    return 'YOUR ACCESS TOKEN SECRET'
+    return 'AxPTgSbh3Pw35pWUULCrK0q0DmDEnTx8Z8dSx5kOMVRCA'
