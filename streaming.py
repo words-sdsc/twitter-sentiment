a3146baa0babad84ba3fd4f445275d2dbcd17cd0
Karl <Kouralr@gmail.com>
2017-08-15 13:46:44 -0700

    Ok

--- streaming.py
+++ streaming.py
@@ -5,17 +5,21 @@ from keys import *
 import time
 import json
 
+import sqlite3
+
 
 #Get input in the form of string
 key_word = input('Enter the search keyword: ')
-print ("STREAMING RESULTS OF #" + key_word)
+print ("Streaming results of %s" % key_word)
 
 #class listener inherting from StreamListener
 class listener(StreamListener):
     def on_data(self, data):
         try:
-            json_obj = json.loads(data)  #Loads for a stirng, load for an object file
+            # Loads for a stirng, load for an object file
+            json_obj = json.loads(data)  
             info = ""
+
             # Selects value of name of lsit user of obj jsonObj
             info +=  json_obj['user']['name']
             info += " tweeted: \n"
@@ -26,6 +30,7 @@ class listener(StreamListener):
 
         except e:
             print( 'failed ondata' , str(e) )
+            return False
 
 def on_error(self, status):
     print (status)
@@ -34,6 +39,7 @@ def on_error(self, status):
 #Initialize our tokens for access
 auth = OAuthHandler(consumer_key(), consumer_secret())
 auth.set_access_token(access_token(), access_token_secret())
+
 #Sets up our twitter streaming using our class
 twitterStream = Stream(auth, listener())
 twitterStream.filter(track=[key_word])
