1eee96458ca4293947515097a5690a3bc8b5c738
Karl <Kouralr@gmail.com>
2017-08-16 16:24:23 -0700

    Tweets sent to JS via sockets

--- streaming.py
+++ streaming.py
@@ -5,41 +5,55 @@ from keys import *
 import time
 import json
 
+import settings
 import sqlite3
 
+from flask_socketio import SocketIO, emit, join_room, leave_room, close_room, rooms, disconnect
 
-#Get input in the form of string
-key_word = input('Enter the search keyword: ')
-print ("Streaming results of %s" % key_word)
+def start(socketio):
+    #Get input in the form of string
+    #key_word = input('Enter the search keyword: ')
+    key_word = "North Korea"
+    print ("Streaming results of %s" % key_word)
+    settings.init()
+    #class listener inherting from StreamListener
+    class listener(StreamListener):
+        def on_data(self, data):
+            try:
+                global stocketio
+                # Loads for a stirng, load for an object file
+                json_obj = json.loads(data)  
+                info = ""
 
-#class listener inherting from StreamListener
-class listener(StreamListener):
-    def on_data(self, data):
-        try:
-            # Loads for a stirng, load for an object file
-            json_obj = json.loads(data)  
-            info = ""
+                # Selects value of name of lsit user of obj jsonObj
+                info +=  json_obj['user']['name']
+                info += " tweeted: \n"
+                info += json_obj['text']
+                info += "\n on " + json_obj['created_at'] + "\n"
+                settings.tweetListBuffer.append(json_obj['text'])
+                
+                socketio.sleep(.5)
+                socketio.emit('my_response',
+                              {'data': json_obj['text'], 'count': 0},
+                              namespace='/test')
 
-            # Selects value of name of lsit user of obj jsonObj
-            info +=  json_obj['user']['name']
-            info += " tweeted: \n"
-            info += json_obj['text']
-            info += "\n on " + json_obj['created_at'] + "\n"
-            print (info)
-            return True
+                #print( info )
+                #print (settings.tweetListBuffer)
+                return True
 
-        except e:
-            print( 'failed ondata' , str(e) )
-            return False
+            except Exception as e:
+                print( "ERROR" )
+                #print( 'failed ondata' , str(e) )
+                #return False
 
-def on_error(self, status):
-    print (status)
+    def on_error(self, status):
+        print (status)
 
-#START OF MAIN, AUTHORIZING OUR DEV POWERS
-#Initialize our tokens for access
-auth = OAuthHandler(consumer_key(), consumer_secret())
-auth.set_access_token(access_token(), access_token_secret())
+    #START OF MAIN, AUTHORIZING OUR DEV POWERS
+    #Initialize our tokens for access
+    auth = OAuthHandler(consumer_key(), consumer_secret())
+    auth.set_access_token(access_token(), access_token_secret())
 
-#Sets up our twitter streaming using our class
-twitterStream = Stream(auth, listener())
-twitterStream.filter(track=[key_word])
+    #Sets up our twitter streaming using our class
+    twitterStream = Stream(auth, listener())
+    twitterStream.filter(track=[key_word])
