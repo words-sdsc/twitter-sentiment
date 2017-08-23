0aa3962ab03b330bc9cfaa14c49c11af944833b9
Karl <Kouralr@gmail.com>
2017-08-23 15:28:07 -0700

    Added additional comments

--- TwitterScraper/TwitterScraper/spiders/searchMiner.py
+++ TwitterScraper/TwitterScraper/spiders/searchMiner.py
@@ -23,12 +23,11 @@ import sys
 # Run scrapy from script
 from scrapy.crawler import CrawlerProcess
 
-# Local imports
-#from insertTweetInfo import insertTweetInfo
-
+# Import Global Variables
 sys.path.append("../../..")
 import settings
 from main import *
+
 
 SPIDER_NAME = "twitter"
 
@@ -36,32 +35,43 @@ SPIDER_NAME = "twitter"
 dbLocation = "sql/TweetInfo.db"
 dbTableName = getTableName()
 
-
 def insertTweetInfo( dbName, dataBaseLoc, tweetId, epoch, tweet, sent ):
+    """ Inserts pack of tweet information into the SQLite database. 
+    
+    Arguments:
+        string  dbName: Name of the databse 
+        string  dataBaseLoc: Path of the database (relative to location of 
+                             method caller is being called)
+        int     tweetId: Tweet ID code
+        int     epoch: Unix timestamp
+        string  tweet: Text of tweet
+        int     sent: Sentiment score
+
+    Return: None
+    """
     # Connect to specified DB
     c = sqlite3.connect(dataBaseLoc)
     
-    print("Inserting tweet info")
-
+    print("Inserting tweet info...")
     # Insert a row of data
     try:
-        c.execute("INSERT INTO " + dbName + " (ID, DATE, TWEET, SENTIMENT) \
-            VALUES ( ?, ?, ?, ? )", ( (tweetId, epoch, tweet, sent) ))
+        c.execute("INSERT INTO " + dbName + " (ID, DATE, TWEET, SENTIMENT) "
+            "VALUES ( ?, ?, ?, ? )", ( (tweetId, epoch, tweet, sent) ))
         c.commit()   
     except:
         print("Error inserting into dataBase %s\nID for reference: %s" % dbName, tweetId)
-    
-    #c.close()
 
 
-class QuotesSpider(scrapy.Spider):
+""" Scrapy spider class """
+class TwitterSpider(scrapy.Spider):
     name = SPIDER_NAME
 
-    # Initialize to front as a Firefox() browser; selenium
     def __init__(self):
         """ Set up selenium driver """
-        self.driver = webdriver.Firefox()
-        #self.driver = webdriver.PhantomJS()
+
+        self.driver = webdriver.Firefox()       # Open firefox browser to see
+        #self.driver = webdriver.PhantomJS()    # Run scraping headless (faster) but can't see
+
         self.driver.implicitly_wait(30)
         self.base_url = "https://twitter.com"
         self.verificationErrors = []
@@ -69,24 +79,47 @@ class QuotesSpider(scrapy.Spider):
 
     # Begin making requests from select URLS
     def start_requests(self):
+        """ Set up URL Requests to be parsed. Note: we don't really utilize this
+            for its proper usage since we use global variables for the URLs 
+            instead.
+        
+        Arguments: None
+
+        Return: None
+        """
+        # URLs to scrape.
         urls = [
-            #'https://twitter.com/search?q=%23WhittierFire&src=tyah',
-            'https://twitter.com/search?q=%23DetwilerFire&src=tyah&lang=en'
+            'https://twitter.com'
         ]
 
-        # Make a request for each url to be parsed below
+        # Make a request for each url and call to be parsed after
         for url in urls:
             yield scrapy.Request(url=url, callback=self.parse)
 
     # Method called after each scrapy request
     def parse(self, response):
-        # Load page for selenium
+        """ Parses the request made from a specified URL. 
+        
+            *Note, we don't use much of the tools Scrapy provides and use 
+            selenium instead. E.g. instead of using response we use 
+            driver.get(getUrlQuery()). Scrapy just laid out good structure, 
+            and we needed selenium for more advanced page scraping.
+        
+        Arguments:
+            response: This response refers to the response of the Request in
+                      start_requests. This is the response from the specific
+                      HTML request.
 
-        #self.driver.get(response.url)
-        print( "URL IS : %s " % getUrlQuery() )
+        Return: None
+        """
+
+        # Load page for selenium
+        print( "Parsing url: \"%s\"" % getUrlQuery() )
         self.driver.get( getUrlQuery() )
+
+        # Attempt to scrape page
         try:
-            # Scroll down the webpage via Selenium (infinite scrolling)
+            # Scroll down the webpage via Selenium (infinite scrolling page)
             for i in range(1,150):
                 self.driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
                 time.sleep(.5)
@@ -115,6 +148,7 @@ class QuotesSpider(scrapy.Spider):
             for timeStamp in unixTimesTagList:
                 unixTimesList.append(timeStamp['data-time'])
             
+            # All list sizes should be the same
             print( len(tweetTextsList) )
             print( len(tweetIdList) )
             print( len(unixTimesList) )
@@ -126,24 +160,28 @@ class QuotesSpider(scrapy.Spider):
                 tweetId = str(tweetId)
                 unixTime = str(unixTime)
 
-                print( getTableName() )
-                print( dbLocation )
-                print( tweetId )
-                print( unixTime )
-                print( tweet )
-
                 # Insert information to DB
                 insertTweetInfo( getTableName(), dbLocation, tweetId, unixTime, tweet, -1 )
             
-            self.driver.close() # Close web page
-        except:
+            # Finished successfully, close driver process
+            Print("Scraping Successful")
             self.driver.close()
 
+        except:
+            # Failed to scrape, close driver process
+            self.driver.close() 
+
 def startCrawler():
+    """ Initiates process of the web crawler above.
+    
+    Arguments: None
+
+    Return: None
+    """
+
+    # Starts a Twisted reactors to configure logs and set shutdown handlers
     process = CrawlerProcess({
         'USER_AGENT': 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)'
     })
-
-    process.crawl(QuotesSpider)
+    process.crawl(TwitterSpider)
     process.start()
-
