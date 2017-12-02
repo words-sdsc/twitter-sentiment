5a79710d12701d808f59bf8c7ee4e48ba9c3381a
Mauricio Panelo <mpanelo@ucsd.edu>
2017-12-01 21:22:21 -0800

    refactored for better readability and allowed keywords to be passed into the stream.filter function

--- streaming.py
+++ streaming.py
@@ -1,81 +1,86 @@
-from tweepy import Stream
-from tweepy import OAuthHandler
-from tweepy.streaming import StreamListener
-from keys import *
-import time
-import json
-
-import settings
-import sqlite3
+import tweepy, json
 
 from SentimentAnalysis.sentimentAnalysis import *
+from flask_socketio import SocketIO, emit
 
-from flask_socketio import SocketIO, emit, join_room, leave_room, close_room, rooms, disconnect
-
-def start(socketio):
-    #Get input in the form of string
-    #key_word = input('Enter the search keyword: ')
-    key_word = "North Korea"
-    print ("Streaming results of %s" % key_word)
-    settings.init()
-    #class listener inherting from StreamListener
-    class listener(StreamListener):
-        def on_data(self, data):
-            try:
-                global stocketio
-                # Loads for a stirng, load for an object file
-                json_obj = json.loads(data)  
-                info = ""
-                sent = 0
-
-                # Selects value of name of lsit user of obj jsonObj
-                info +=  json_obj['user']['name']
-                info += " tweeted: \n"
-                info += json_obj['text']
-                info += "\n on " + json_obj['created_at'] + "\n"
-                settings.tweetListBuffer.append(json_obj['text'])
-
-                # Collect info
-                #date = json_obj['created_at']
-
-                retweet = False
-                tweetText = json_obj['text']
-
-                if "RT @" in tweetText:
-                    retweet = True
-
-                tweetText = tweetText.replace("RT @", "")
-                sent = getSentiment(tweetText)
+import settings
 
 
-                # Must have socket sleep to return info
-                socketio.sleep(.5)
+class StreamListener(tweepy.StreamListener):
 
-                # Send information of tweets through socket 
-                socketio.emit('my_response',
-                              {'data': tweetText, 
-                              'count': 0,
-                              'sentiment': sent,
-                              'retweet' : retweet },
-                              namespace='/test')
+    def __init__(self, socketio):
+        super().__init__()
+        self.socketio = socketio
 
-                #print( info )
-                #print (settings.tweetListBuffer)
-                return True
+    def on_connect(self):
+        print("Successfully connected to the Twitter Streaming API")
 
-            except Exception as e:
-                print( "ERROR", str(e) )
-                #print( 'failed ondata' , str(e) )
-                #return False
+    def on_data(self, data):
+        json_data = json.loads(data)
 
-    def on_error(self, status):
-        print (status)
+        tweet_id = json_data['id']
+        text = json_data['text']
+        sentiment = getSentiment(text)
 
-    #START OF MAIN, AUTHORIZING OUR DEV POWERS
-    #Initialize our tokens for access
-    auth = OAuthHandler(consumer_key(), consumer_secret())
-    auth.set_access_token(access_token(), access_token_secret())
+        retweeted = True if "RT @" in text else False
+        if retweeted: 
+            text = text.replace("RT @", "")
 
-    #Sets up our twitter streaming using our class
-    twitterStream = Stream(auth, listener())
-    twitterStream.filter(track=[key_word])
+        self.socketio.emit("response",
+                           { "text": text,
+                             "sentiment": sentiment,
+                             "tweet_id": tweet_id,
+                             "retweeted": retweeted },
+                           namespace="/streaming")
+
+    def on_error(self, status_code):
+        print('ERROR', status_code)
+        if status_code == 420:
+            return False
+
+
+class TwitterStream():
+
+    def __init__(self, socketio):
+        manager = CredentialsManager(settings.CREDENTIALS_PATH)
+        cred = manager.read()
+
+        auth = tweepy.OAuthHandler(cred['twitter_api_key'] ,
+                                   cred['twitter_api_secret'])
+
+        auth.set_access_token(cred['access_token'],
+                              cred['access_token_secret'])
+        self.stream = tweepy.Stream(auth, StreamListener(socketio))
+
+    def flow(self, hashtag):
+        self.stream.filter(track=[hashtag])
+
+
+class CredentialsManager():
+
+    def __init__(self, path):
+        self._path = path
+
+    def read(self):
+        c = Credentials()
+
+        with self._path.open() as f:
+            for line in f:
+                k, v = line.strip().split('=')
+                c[k] = v
+
+        return c
+
+    def write(self, credentials):
+        with self._path.open('w') as f:
+            for k, v in credentials.items():
+                f.write(k + '=' + v + '\n')
+
+
+class Credentials(dict):
+
+    def __init__(self):
+        super(Credentials, self).__init__(
+            { 'twitter_api_key': 'None', 'twitter_api_secret': 'None',
+              'access_token': 'None', 'access_token_secret': 'None' }
+        )
