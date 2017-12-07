2dfb1a2b158bacd2ed9df8502ed66f9ea4d2a70f
Mauricio Panelo <mpanelo@ucsd.edu>
2017-12-06 22:47:08 -0800

    refer to the previous commit message

--- static/js/streaming.js
+++ static/js/streaming.js
@@ -1,22 +1,126 @@
-$(document).ready(function() {
-  var chart = Highcharts.chart('container', {
+var Colors = {
+  // Negative - Red
+  red: 'rgba(247, 46, 70, .5)',
+
+  // Neutral - Blue
+  blue: 'rgba(81, 185, 255, .5)',
+
+  // Positive - Green
+  green: 'rgba(48, 229, 81, .5)'
+};
+
+var map;
+var chart;
+
+var namespace = '/streaming';
+var protocol = location.protocol;
+var port = location.port;
+var domain = document.domain;
+
+// Connect to the Socket.IO server.
+// The connection URL has the following format:
+//     http[s]://<domain>:<port>[/<namespace>]
+var socket = io.connect(protocol + '//' + domain + ':' + port + namespace);
+
+var drawnItems = new L.FeatureGroup();
+var drawOptions = {
+  position: 'topleft',
+  draw: {
+    polyline: false,
+    polygon:  false,
+    circle:   false,
+    rectangle: {
+      shapeOptions: { color: '#5A0888', weight: 4 }
+    }
+  },
+  edit: {
+    featureGroup: drawnItems
+  }
+};
+var drawnControl = new L.Control.Draw(drawOptions);
+
+var HashTagControl = L.Control.extend({
+  options: {
+    position: 'topright',
+    type: 'text',
+    name: 'hashtag',
+    placeholder: 'Twitter Hashtag (e.g. #sdsc)'
+  },
+  onAdd: function (map) {
+    var input = L.DomUtil.create('input', 'hashtag-input');
+
+    input.type = this.options.type;
+    input.placeholder = this.options.placeholder;
+    input.name = this.options.name;
+
+    L.DomEvent.disableClickPropagation(input);
+
+    L.DomEvent.on(input, 'keydown', function(e) {
+      if (e.keyCode === 13) {
+        socket.emit('start_stream', { hashtag : input.value });
+      }
+    });
+
+    return input;
+  }
+});
+var hashtagControl = new HashTagControl();
+
+var ChartControl = L.Control.extend({
+  options: {
+    position: 'bottomright'
+  },
+  onAdd: function (map) {
+    var div = L.DomUtil.create('div');
+    div.id = 'highcharts-chart-container';
+    return div
+  }
+});
+var chartControl = new ChartControl();
+
+$(function() {
+  /* Leaflet */
+  map = L.map('leaflet-map-container', { center: [38.4404, -122.7141], zoom: 13});
+
+  L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+      maxZoom: 18,
+      attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
+      '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+      'Imagery © <a href="http://mapbox.com">Mapbox</a>',
+      id: 'mapbox.streets',
+      accessToken: 'pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw'
+  }).addTo(map);
+
+  map.addLayer(drawnItems);
+  map.addControl(drawnControl);
+  map.addControl(hashtagControl);
+  map.addControl(chartControl);
+
+  map.on(L.Draw.Event.CREATED, function (e) {
+    if (e.layerType === 'rectangle')
+      console.log(e.layer.getLatLngs());
+    drawnItems.addLayer(e.layer);
+  });
+
+  /* Highcharts */
+  chart = Highcharts.chart('highcharts-chart-container', {
+    credits: {
+      enabled: false
+    },
     // Declare chart type and basic information
     chart: {
       type: 'scatter',
-      zoomType: 'xy'
+      zoomType: '',
+      height: 400,
+      width: 500
     },
     title: {
       text: 'Sentiment Over Time '
     },
-    subtitle: {
-      text: 'Made with <3'
-    },
-
     // Set up xAxis Attributes
     xAxis: {
       type: 'datetime',
       title: {
-        enabled: true,
         text: 'Date'
       },
       startOnTick: true,
@@ -24,21 +128,13 @@ $(document).ready(function() {
       showLastLabel: true
     },
     yAxis: {
+      lineWidth: 1,
+      min: -100,
+      max: 100,
       title: {
         text: 'Sentiment'
       }
     },
-    legend: {
-      layout: 'vertical',
-      align: 'left',
-      verticalAlign: 'top',
-      x: 100,
-      y: 70,
-      floating: true,
-      backgroundColor: '#FFFFFF',
-      borderWidth: 1
-    },
-
     plotOptions: {
       scatter: {
         // Allows for dictionary format despite high data amount > 1000
@@ -65,11 +161,11 @@ $(document).ready(function() {
         point: {
           events: {
             click: function() {
-              alert("Tweet Text:\n" + this.tweetText);
+              alert("Tweet Text:\n" + this.text);
               console.log("This x = " + this.x);
               console.log("This y = " + this.y)
-                console.log("This tweetID = " + this.tweetId);
-              console.log("This tweetText = " + this.tweetText)
+              //console.log("This tweetID = " + this.tweetId);
+              console.log("This tweetText = " + this.text)
             }
           }
         }
@@ -83,20 +179,20 @@ $(document).ready(function() {
       // Change colors based on POSITIVE, NEGATIVE, NEUTRAL
       zones: [
       {
-        // Negatie - Red
+        // Negative
         value: -5,
-        color: 'rgba(247, 46, 70, .5)' 
+        color: Colors.red
       }, {
-        // Neutral - Blue
+        // Neutral
         value: 5,
-        color: 'rgba(81, 185, 255, .5)'
+        color: Colors.blue
       }, {
-        // Positive - Green
-        color: 'rgba(48, 229, 81, .5)'
+        // Positive
+        color: Colors.green
       }
       ],
 
-      pointInterval: 86400000,
+      //pointInterval: 1000,
 
       // Pointer description
       pointDescriptionFormatter: function() {
@@ -119,20 +215,20 @@ $(document).ready(function() {
       // Change colors based on POSITIVE, NEGATIVE, NEUTRAL
       zones: [
       {
-        // Negatie - Red
+        // Negatie
         value: -5,
-        color: 'rgba(247, 46, 70, .5)' 
+        color: Colors.red
       }, {
-        // Neutral - Blue
+        // Neutral
         value: 5,
-        color: 'rgba(81, 185, 255, .5)'
+        color: Colors.blue
       }, {
-        // Positive - Green
-        color: 'rgba(48, 229, 81, .5)'
+        // Positive
+        color: Colors.green
       }
       ],
 
-      pointInterval: 86400000,
+      //pointInterval: 1000,
 
       // Pointer description
       pointDescriptionFormatter: function() {
@@ -154,44 +250,24 @@ $(document).ready(function() {
 
   /* Websockets */
 
-  // Use a "/test" namespace.
-  // An application can open a connection on multiple namespaces, and
-  // Socket.IO will multiplex all those connections on a single
-  // physical channel. If you don't care about multiple channels, you
-  // can set the namespace to an empty string.
-  var namespace = '/streaming'
-    , protocol = location.protocol
-    , port = location.port
-    , domain = document.domain;
-
-  // Connect to the Socket.IO server.
-  // The connection URL has the following format:
-  //     http[s]://<domain>:<port>[/<namespace>]
-  var socket = io.connect(protocol + '//' + domain + ':' + port + namespace);
-
-  // Event handler for new connections.
-  // The callback function is invoked when a connection with the
-  // server is established.
-  $("#hashtag").keydown(function(e) {
-    if (e.keyCode === 13) {
-      socket.emit('start_stream', { hashtag : $(this).val() });
-    }
-  });
-
   // Event handler for server sent data.
   // The callback function is invoked whenever the server emits data
   // to the client. The data is then displayed in the "Received"
   // section of the page.
   socket.on('response', function(msg) {
     $('#log').append('<br>' + $('<div/>')
-             .text('Received #' + msg.sentiment + ': ' + msg.text)
+             .text('Received #' + msg.sentiment + ': ' + msg.text + " AT " + msg.created_at)
              .html());
     console.log("Retweeted: " + msg.retweeted);
+
+    var milliseconds = Date.parse(msg.created_at);
     // Dynamically add points to the first series
     if( msg.retweeted ) {
-      chart.series[1].addPoint(msg.sentiment * 100, true, false, true);
+      chart.series[1].addPoint([milliseconds, msg.sentiment * 100]);
+      //chart.series[1].addPoint(msg.sentiment * 100, true, false, true);
     } else {
-      chart.series[0].addPoint(msg.sentiment * 100, true, false, true);
+      chart.series[0].addPoint([milliseconds, msg.sentiment * 100]);
+      //chart.series[0].addPoint(msg.sentiment * 100, true, false, true);
     }
   });
 
